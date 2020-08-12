b6513d66933307a76915b1d8e65b4aac7f31e75d
Ajay Charotariya <ajay.charotariya@aspiresoftserv.com>
2020-08-13 01:35:37 +0530

    fixed dropdown disappear issues & tslint

--- projects/test-library-app/src/app/components/currency-conversion/currency-conversion.component.ts
+++ projects/test-library-app/src/app/components/currency-conversion/currency-conversion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CurrencyConversionService } from 'angular-currency-converter';
-import { NgbDateStruct,NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-currency-conversion',
@@ -11,28 +11,28 @@ import { NgbDateStruct,NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 export class CurrencyConversionComponent implements OnInit {
   model: NgbDateStruct;
   myForm: FormGroup;
-  public amount: Number;
-  public baseCurrencyRate: string; // base currencies rate  
+  public amount: number;
+  public baseCurrencyRate: string; // base currencies rate
   public baseCurrencyCode: string; // base currency code
-  public convertedRates: any = []; // store converted 
+  public convertedRates: any = []; // store converted
   public currencyRates: any = [];  // store data from currency rates
   public apisSource: any;          // apis source for source currencies rates dropdown
-  public disabled: boolean;        // disable calender icon 
+  public disabled: boolean;        // disable calender icon
   public selectedSource: string;   // selected source
   public selectedBase: string;
   public selectedTarget: string;
   public submitted: boolean;       // handle form submit
-  public apisDetails: any;         // api configurations 
+  public apisDetails: any;         // api configurations
   public host: any;                // host api
   public isVisible: boolean;       // table visibility
   public dateError: boolean;       // show date error
 
-  constructor(private currencyConversionService: CurrencyConversionService,private calendar: NgbCalendar) {
+  constructor(private currencyConversionService: CurrencyConversionService, private calendar: NgbCalendar) {
   }
 
   ngOnInit() {
 
-    this.isVisible = false; 
+    this.isVisible = false;
     this.disabled = true;
     this.dateError = false;
     this.currencyRates = [];
@@ -47,101 +47,115 @@ export class CurrencyConversionComponent implements OnInit {
     });
 
     /* Get apisConfigurations */
-    this.apisDetails = this.currencyConversionService.getApisConfigurations()
-    this.host = this.apisDetails.host
-    this.apisSource = this.apisDetails.currencyRateSource
+    this.apisDetails = this.currencyConversionService.getApisConfigurations();
+    this.host = this.apisDetails.host;
+    this.apisSource = this.apisDetails.currencyRateSource;
 
-    
+
     /* Get latest currencies rate [ default base: USD ] */
     this.currencyConversionService.getCurrencyRates(`${this.host}/latest?base=USD`).subscribe(data => {
       if (data) {
-        for (var key in data['rates']) {
-          if (data['rates'].hasOwnProperty(key)) {
-            this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
+        for (const key of Object.keys(data.rates)) {
+          if (data.rates.hasOwnProperty(key)) {
+            const currencyInfo = {
+              currencyCode: key,
+              rate: data.rates[key],
+              defaultBase: data.base
+            };
+            this.currencyRates.push(currencyInfo);
           }
         }
-        this.sortCurrencyCode(this.currencyRates)
+        this.sortCurrencyCode(this.currencyRates);
       }
     }, error => {
-      console.log("Error", error)
-    })
+      console.log('Error', error);
+    });
   }
 
   /* On source change  */
   async onSourceChange(event) {
-    this.dateError= false;
-    this.selectedSource = event.target.options[event.target.options.selectedIndex].text
-    if (this.selectedSource == 'History') { // On select history user have to apply date
-      this.myForm.controls['datePicker'].enable();
+    this.dateError = false;
+    this.selectedSource = event.target.options[event.target.options.selectedIndex].text;
+    if (this.selectedSource === 'History') { // On select history user have to apply date
+      this.myForm.controls.datePicker.enable();
       this.disabled = false;
     } else {
       /* Get latest currencies rate by selection of latest option [ default base: USD ] */
-      this.myForm.controls['datePicker'].setValue(undefined);
-      this.myForm.controls['datePicker'].disable();
+      this.myForm.controls.datePicker.setValue(undefined);
+      this.myForm.controls.datePicker.disable();
       this.disabled = true;
-      this.currencyRates = [];
+      // this.currencyRates = [];
       this.currencyConversionService.getCurrencyRates(`${this.host}/latest?base=USD`).subscribe(data => {
         if (data) {
-          for (var key in data['rates']) {
-            if (data['rates'].hasOwnProperty(key)) {
-              this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
+          // tslint:disable-next-line:prefer-for-of
+          for (let index = 0; index < this.currencyRates.length; index++) {
+            const element = this.currencyRates[index];
+            for (const key of Object.entries(data.rates)) {
+              if (this.currencyRates[index].currencyCode === key[0]) {
+                this.currencyRates[index].rate = key[1];
+              }
             }
-            this.sortCurrencyCode(this.currencyRates)
           }
+          this.sortCurrencyCode(this.currencyRates);
         }
       }, error => {
-        console.log("Error", error)
-      })
+        console.log('Error', error);
+      });
 
     }
   }
 
-  onBaseCurrencyChange(event){
-    this.selectedBase = "";
-    this.selectedBase = event.target.options[event.target.options.selectedIndex].text
+  onBaseCurrencyChange(event) {
+    this.selectedBase = '';
+    this.selectedBase = event.target.options[event.target.options.selectedIndex].text;
   }
 
-  onTargetCurrencyChange(event){
-    this.selectedTarget = "";
-    this.selectedTarget = event.target.options[event.target.options.selectedIndex].text
+  onTargetCurrencyChange(event) {
+    this.selectedTarget = '';
+    this.selectedTarget = event.target.options[event.target.options.selectedIndex].text;
   }
 
   /* On date change get currencies rate of specified date  */
   async onDateChange(model) {
     this.dateError = false;
-    if (model !== undefined) {
+    // if (model !== undefined) {
       // Display error on selection of today & future dates [date picker]
-     
-      let getToday = this.calendar.getToday()
-      let todayDate = Date.parse(`${getToday.year}-${getToday.month}-${getToday.day}`);
-      let date = `${model.year}-${model.month}-${model.day}`
-      let selectedDate = Date.parse(date)
-      this.validateDate(selectedDate,todayDate)
-      
-      
-      this.currencyRates = [];
+
+    const getToday = this.calendar.getToday();
+    const todayDate = Date.parse(`${getToday.year}-${getToday.month}-${getToday.day}`);
+    const date = `${model.year}-${model.month}-${model.day}`;
+    const selectedDate = Date.parse(date);
+    this.validateDate(selectedDate, todayDate);
+
+
+    // this.currencyRates = [];
       // Get currency rate history of selected date
-      let url = `${this.host}/${date}?base=USD`
-      this.currencyConversionService.getCurrencyRates(url).subscribe(data => {
+    const url = `${this.host}/${date}?base=USD`;
+    this.currencyConversionService.getCurrencyRates(url).subscribe(data => {
         if (data) {
-          for (var key in data['rates']) {
-            if (data['rates'].hasOwnProperty(key)) {
-              this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
+          // tslint:disable-next-line:prefer-for-of
+          for (let index = 0; index < this.currencyRates.length; index++) {
+            const element = this.currencyRates[index];
+            for (const key of Object.entries(data.rates)) {
+              if (this.currencyRates[index].currencyCode === key[0]) {
+                this.currencyRates[index].rate = key[1];
+              }
             }
           }
-          this.sortCurrencyCode(this.currencyRates)
+          this.sortCurrencyCode(this.currencyRates);
+
         }
       }, error => {
-        console.log("Error", error)
-      })
-    }
+        console.log('Error', error);
+      });
+    // }
   }
 
  /* Date validation */
-  
+
   validateDate(selectedDate, todayDate) {
     if (selectedDate >= todayDate) {
-      this.dateError = true
+      this.dateError = true;
       return;
     }
   }
@@ -154,48 +168,49 @@ export class CurrencyConversionComponent implements OnInit {
       this.submitted && this.myForm.get(field).errors
     );
   }
- 
+
   /* Get submitted form values */
   async onSubmit(form: FormGroup) {
     this.submitted = true;
     this.convertedRates = [];
-    if (this.myForm.invalid && (form.value.datePicker == undefined)) {
-      this.isVisible= false;
-      if(form.value.targetSource.value == 'history'){
+    if (this.myForm.invalid && (form.value.datePicker === undefined)) {
+      this.isVisible = false;
+      if (form.value.targetSource.value === 'history') {
         this.dateError = true;
-      }else{
+      } else {
         this.dateError = false;
       }
       return;
     } else {
       this.isVisible = true;
-      if(form.value.targetSource.value === 'history' && form.value.datePicker == undefined){
-        this.dateError= true;
-        this.isVisible= false;
+      if (form.value.targetSource.value === 'history' && form.value.datePicker === undefined) {
+        this.dateError = true;
+        this.isVisible = false;
         return;
       }
-      let amount = form.value.amount;
-      let baseCurrencyRate = form.value.countryRates;
-      let baseCurrencyCode = this.selectedBase;
-      let targetCurrencyRate = form.value.targetRates;
-      let targetCurrencyCode = this.selectedTarget;
+      const amount = form.value.amount;
+      const baseCurrencyRate = form.value.countryRates.rate;
+      const baseCurrencyCode = this.selectedBase;
+      const targetCurrencyRate = form.value.targetRates.rate;
+      const targetCurrencyCode = this.selectedTarget;
+      // tslint:disable-next-line:max-line-length
       this.convertedRates = await this.currencyConversionService.convertCurrency(amount, baseCurrencyRate, baseCurrencyCode, targetCurrencyRate, targetCurrencyCode);
       this.submitted = false;
     }
   }
 
-  sortCurrencyCode(data){
-    data.sort(function(a, b){
-      if(a.currencyCode < b.currencyCode) { return -1; }
-      if(a.currencyCode > b.currencyCode) { return 1; }
+  sortCurrencyCode(data) {
+    data.sort((a, b) => {
+      if (a.currencyCode < b.currencyCode) { return -1; }
+      if (a.currencyCode > b.currencyCode) { return 1; }
       return 0;
-  })
+  });
   }
 
   /* Reset form after submit values */
   resetForm() {
-    this.selectedBase = undefined
-    this.selectedTarget = undefined
+    this.selectedBase = undefined;
+    this.selectedTarget = undefined;
     this.isVisible = false;
     this.submitted = false;
     this.myForm = new FormGroup({
